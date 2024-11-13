a650fbfb71638e2aad1783b61d4a7c01d82d4af3
Mane <manemanoj850@gmail.com>
2024-11-13 23:19:58 +0530

    params & Routes

--- src/app.js
+++ src/app.js
@@ -3,19 +3,27 @@ const app=express()
 // app.use("/test",(req,res)=>{
 //    res.send("hellow world is here")
 // })
-// app.use("/12",(req,res)=>{
-//     res.send("its a number series")
-// })
+app.get("/user/:userid",(req,res)=>{
+    console.log(req.params)
+    res.send({firstname:"Mane",lastName:"Manoj"})
+//to read the Dynamic routes http://localhost:7777/user/19
+})
 
-app.get("/login",(req,res)=>{
-    res.send("its a login data")
+app.get("/user",(req,res)=>{
+    console.log(req.query)
+    res.send({firstname:"mane",lastName:"manoj"})
+    // to read the url http://localhost:7777/user?userid=561&password=1
 })
-app.post("/login",(req,res)=>{
-    res.send("its a post data")
-})
-app.delete("/login",(req,res)=>{
-    res.send("its a delete data")
-})
+
+// app.get("/login",(req,res)=>{
+//     res.send("its a login data")
+// })
+// app.post("/login",(req,res)=>{
+//     res.send("its a post data")
+// })
+// app.delete("/login",(req,res)=>{
+//     res.send("its a delete data")
+// })
 
 app.listen(7777,()=>{
     console.log("server is running on port 7777");
