1d40523c000e1996532634c8c67a3300a3b322a4
Mane <manemanoj850@gmail.com>
2024-11-13 22:27:44 +0530

    exlporing a Git,Post & Delete Methods

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 const express=require("express");
 const app=express()
-app.use("/test",(req,res)=>{
-   res.send("hellow world is here")
+// app.use("/test",(req,res)=>{
+//    res.send("hellow world is here")
+// })
+// app.use("/12",(req,res)=>{
+//     res.send("its a number series")
+// })
+
+app.get("/login",(req,res)=>{
+    res.send("its a login data")
 })
-app.use("/12",(req,res)=>{
-    res.send("its a number series")
+app.post("/login",(req,res)=>{
+    res.send("its a post data")
 })
+app.delete("/login",(req,res)=>{
+    res.send("its a delete data")
+})
+
 app.listen(7777,()=>{
     console.log("server is running on port 7777");
 })
