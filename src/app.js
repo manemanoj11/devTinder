f8cd6ef409e6a22ca0b97b9d97b04ecaf4d1bab3
Mane <manemanoj850@gmail.com>
2024-11-17 17:39:48 +0530

    middleWares

--- src/app.js
+++ src/app.js
@@ -1,30 +1,15 @@
 const express=require("express");
 const app=express()
-// app.use("/test",(req,res)=>{
-//    res.send("hellow world is here")
-// })
-app.get("/user/:userid",(req,res)=>{
-    console.log(req.params)
-    res.send({firstname:"Mane",lastName:"Manoj"})
-//to read the Dynamic routes http://localhost:7777/user/19
+//middleware
+app.use("/",(req,res,next)=>{
+    console.log("Hello from middleware")
+   // res.send("response")
+    next()
 })
-
 app.get("/user",(req,res)=>{
-    console.log(req.query)
-    res.send({firstname:"mane",lastName:"manoj"})
-    // to read the url http://localhost:7777/user?userid=561&password=1
+    res.send("from middleware")
+    console.log("Hello from middleware")
 })
-
-// app.get("/login",(req,res)=>{
-//     res.send("its a login data")
-// })
-// app.post("/login",(req,res)=>{
-//     res.send("its a post data")
-// })
-// app.delete("/login",(req,res)=>{
-//     res.send("its a delete data")
-// })
-
 app.listen(7777,()=>{
     console.log("server is running on port 7777");
 })
