2d56be013c6480dc9fb2b14b47ecb2bc876f0dce
Mane <manemanoj850@gmail.com>
2024-11-18 23:01:40 +0530

    need of middleware

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const express=require("express");
 const app=express()
 //middleware
-app.use("/",(req,res,next)=>{
-    console.log("Hello from middleware")
-   // res.send("response")
-    next()
+const {adminAuth,userAuth}=require("./middlewares/auth")
+app.use("/admin",adminAuth)
+
+app.post("/User/login",(req,res)=>{
+    res.send("User logged in sucessfully")
 })
-app.get("/user",(req,res)=>{
-    res.send("from middleware")
-    console.log("Hello from middleware")
+app.get("/user/data",userAuth,(req,res)=>{
+    res.send("User data send")
+})
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("All Data sent")
 })
 app.listen(7777,()=>{
     console.log("server is running on port 7777");
